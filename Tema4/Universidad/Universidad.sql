7a83226678258f2e4a2a45c98f42bd4c198425e4
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-01-16 18:04:45 +0100

    commit

--- Tema4/Universidad/Universidad.sql
+++ Tema4/Universidad/Universidad.sql
@@ -16,21 +16,21 @@ CREATE TABLE IF NOT EXISTS DEPTOS (
     CONSTRAINT PK_DEPTOS PRIMARY KEY (numDepto)
 );
 CREATE TABLE IF NOT EXISTS PROFESORES (
-    numDepto varchar(30) not null,
+    numDepto VARCHAR(30) NOT NULL,
     numProf VARCHAR(30) NOT NULL,
     despacho VARCHAR(30) NOT NULL,
     fecNacim DATETIME NULL,
     fecIngreso DATETIME NOT NULL,
     sueldo DECIMAL(6 , 2 ) NOT NULL,
     nomProfe VARCHAR(60) NOT NULL,
-    idJefe varchar(30) null,
+    idJefe VARCHAR(30) NULL,
     CONSTRAINT PK_PROFESORES PRIMARY KEY (numProf),
-     constraint FK_PROFESORES_DEPTOS foreign key(numDepto)
-		references DEPTOS (numDepto)
-        on delete no action on update cascade,
-        constraint FK_PROFESORES_JEFES foreign key(idJefe)
-		references PROFESORES (numProf)
-        on delete no action on update cascade 
+    CONSTRAINT FK_PROFESORES_DEPTOS FOREIGN KEY (numDepto)
+        REFERENCES DEPTOS (numDepto)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT FK_PROFESORES_JEFES FOREIGN KEY (idJefe)
+        REFERENCES PROFESORES (numProf)
+        ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 create table if not exists IMPARTEN
