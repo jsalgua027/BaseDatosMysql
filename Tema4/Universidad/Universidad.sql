a54acc124fad6632d6e959f708d8d3834dcd4179
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-01-16 16:02:38 +0100

    terminado ejercicio 2

--- Tema4/Universidad/Universidad.sql
+++ Tema4/Universidad/Universidad.sql
@@ -8,14 +8,29 @@ CREATE TABLE IF NOT EXISTS ASIGNATURAS (
     CONSTRAINT PK_ASIGNATURAS PRIMARY KEY (numAsigna)
 );
 
+CREATE TABLE IF NOT EXISTS DEPTOS (
+    numDepto VARCHAR(30) NOT NULL,
+    presupuesto DECIMAL(8 , 2 ) NOT NULL,
+    nomDepto VARCHAR(60),
+    ubicacion VARCHAR(60),
+    CONSTRAINT PK_DEPTOS PRIMARY KEY (numDepto)
+);
 CREATE TABLE IF NOT EXISTS PROFESORES (
+    numDepto varchar(30) not null,
     numProf VARCHAR(30) NOT NULL,
     despacho VARCHAR(30) NOT NULL,
     fecNacim DATETIME NULL,
     fecIngreso DATETIME NOT NULL,
     sueldo DECIMAL(6 , 2 ) NOT NULL,
     nomProfe VARCHAR(60) NOT NULL,
-    CONSTRAINT PK_PROFESORES PRIMARY KEY (numProf)
+    idJefe varchar(30) null,
+    CONSTRAINT PK_PROFESORES PRIMARY KEY (numProf),
+     constraint FK_PROFESORES_DEPTOS foreign key(numDepto)
+		references DEPTOS (numDepto)
+        on delete no action on update cascade,
+        constraint FK_PROFESORES_JEFES foreign key(idJefe)
+		references PROFESORES (numProf)
+        on delete no action on update cascade 
 );
 
 create table if not exists IMPARTEN
@@ -34,3 +49,4 @@ create table if not exists IMPARTEN
         on delete no action on update cascade 
 
 );
+
