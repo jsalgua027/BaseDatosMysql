3e9e4d9211ec79cfbbc6bb138181e760972224c5
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-01-31 18:05:47 +0100

    terminado simulacro

--- Tema4/Simulacro examenes/21.sql
+++ Tema4/Simulacro examenes/21.sql
@@ -41,7 +41,7 @@ create table if not exists clientela
      codsujeto INT UNSIGNED NOT NULL,
      estadoCivil enum('S', 'V', 'D', 'C') not null,
      
-     constraint pk_clientela primary key (codclien),
+     constraint pk_clientela primary key (codcli),
      constraint fk_clientela_sujetos foreign key (codsujeto)
      references sujetos (codsujeto)
 	on delete no action on update cascade
@@ -65,11 +65,45 @@ create table if not exists abogado
 CREATE TABLE IF NOT EXISTS tipocasos (
     codtipcaso INT UNSIGNED NOT NULL,
     desTipoCaso VARCHAR(60) NOT NULL,
-    CONSTRAINT pk_tiposcasos PRIMARY KEY (codTipocaso)
+    CONSTRAINT pk_tiposcasos PRIMARY KEY (codtipcaso)
 );
 
+-- casos(PK(codcaso,codtipocaso*),descaso,codcli*,presupuesto)
 
+CREATE TABLE IF NOT EXISTS casos (
+    codcaso INT UNSIGNED NOT NULL,
+    codtipcaso INT UNSIGNED NOT NULL,
+    descaso VARCHAR(100) NULL,
+    codcli INT UNSIGNED NOT NULL,
+    presupesto DECIMAL(7 , 2 ) NOT NULL,
+    CONSTRAINT pk_casos PRIMARY KEY (codcaso , codtipcaso),
+    CONSTRAINT fk_casos_tipocasos FOREIGN KEY (codtipcaso)
+        REFERENCES tipocasos (codtipcaso)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT fk_casos_clientela FOREIGN KEY (codcli)
+        REFERENCES clientela (codcli)
+        ON DELETE NO ACTION ON UPDATE CASCADE
+);
 
+/*
 
+A veces, un abogado vuelve a llevar el mismo caso en periodos de tiempo diferentes. Por esta linea se mete fecinicio como clave primaria tambien
 
+AbogadosenCasos(PK[[codcaso,codtipocaso]*,codabogado*,fecinicio], numdias)
 
+*/
+
+CREATE TABLE IF NOT EXISTS AbogadosenCasos (
+    codcaso INT UNSIGNED NOT NULL,
+    codtipcaso INT UNSIGNED NOT NULL,
+    codabogado INT UNSIGNED NOT NULL,
+    fecinicio DATE NOT NULL,
+    numdias INT UNSIGNED NULL,
+    CONSTRAINT pk_AbogadosenCasos PRIMARY KEY (codcaso , codtipcaso , codabogado , fecinicio),
+    CONSTRAINT fk_AbogadosenCasos_casos FOREIGN KEY (codcaso , codtipcaso)
+        REFERENCES casos (codcaso , codtipcaso)
+        ON DELETE CASCADE ON UPDATE NO ACTION,
+    CONSTRAINT fk_abogado FOREIGN KEY (codabogado)
+        REFERENCES abogado (codabogado)
+        ON DELETE CASCADE ON UPDATE NO ACTION
+);
