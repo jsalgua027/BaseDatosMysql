a946ed85adec243bde4b7048380524ebe84e26d5
jsalgua027 <jsalcedoguarde@gmail.com>
2023-03-06 11:57:59 +0100

    Iniciado ejercicios Unidad6_Actividad1

---
+++ Tema6/Unidad6Actividad1.sql
@@ -0,0 +1,16 @@
+use empresaclase;
+/*
+Para la BD Empresa_clase descargada de la plataforma obtener las siguientes consultas:
+1.Obtener todos los datos de todos los empleados.
+2.Obtener la extensión telefónica de “Juan López”.
+3.Obtener el nombre completo de los empleados que tienen más de un hijo.
+4.Obtener el nombre completo y en una sola columna de los empleados que tienen entre 1 y 3 hijos.
+5.Obtener el nombre completo y en una sola columna de los empleados sin comisión.
+6.Obtener la dirección del centro de trabajo “Sede Central”.
+7.Obtener el nombre de los departamentos que tienen más de 6000 € de presupuesto.
+8.Obtener el nombre de los departamentos que tienen de presupuesto 6000 € o más.
+9.Obtener el nombre completo y en una sola columna de los empleados que llevan trabajando en nuestra empresa más de 1 año. (Añade filas nuevas para poder comprobar que tu consulta funciona).
+10.Obtener el nombre completo y en una sola columna de los empleados que llevan trabajando en nuestra empresa entre 1 y tres años. (Añade filas nuevas para poder comprobar que tu consulta funciona).
+
+
+*/
