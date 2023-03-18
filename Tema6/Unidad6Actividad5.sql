a4980d9f749c012930639558208686dc4cb45a8c
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-03-18 20:53:54 +0100

    ....

--- Tema6/Unidad6Actividad5.sql
+++ Tema6/Unidad6Actividad5.sql
@@ -31,22 +31,65 @@ Para BD_Almacen, hacer los siguientes ejercicios utilizando procedimientos almac
 
 TODOS LOS EJERCICIOS DEBEN HACERSE UTILIZANDO FUNCIONES ESCALARES DE MYSQL
 
-Obtener todos los productos que comiencen por una letra determinada.
-1.Se ha diseñado un sistema para que los proveedores puedan acceder a ciertos datos, 
+1.Obtener todos los productos que comiencen por una letra determinada.
+2.Se ha diseñado un sistema para que los proveedores puedan acceder a ciertos datos, 
 la contraseña que se les da es el teléfono de la empresa al revés. 
 Se pide elaborar un procedimiento almacenado que dado un proveedor obtenga su contraseña y la muestre en los resultados.
 2.Obtener el mes previsto de entrega para los pedidos que no se han recibido aún y para una categoría determinada.
-3.Obtener un listado con todos los productos, ordenados por categoría, en el que se muestre solo las tres primeras letras de la categoría.
-4.Obtener el cuadrado y el cubo de los precios de los productos.
-5.Devuelve la fecha del mes actual.
-6.Para los pedidos entregados el mismo mes que el actual, obtener cuantos días hace que se entregaron.
-7.En el nombre de los productos, sustituir “tarta” por “pastel”.
-8.Obtener la población del código postal (los primeros dos caracteres se refieren a la provincia y los tres últimos a la población).
-9.Obtén el número de productos de cada categoría, haz que el nombre de la categoría se muestre en mayúsculas.
-10.Obtén un listado de productos por categoría y dentro de cada categoría del nombre de producto más corto al más largo.
-11.Asegúrate de que los nombres de los productos no tengan espacios en blanco al principio o al final de dicho nombre.
-12.Lo mismo que en el ejercicio 2, pero ahora, además, sustituye el 4 y 5 número del resultado por las 2 últimas letras del nombre de la empresa.
-13.Obtén el 10 por ciento del precio de los productos redondeados a dos decimales.
-14.Muestra un listado de productos en que aparezca el nombre del producto y el código de producto y el de categoría repetidos (por ejemplo para la tarta de azucar se mostrará “623623”).
+4.Obtener un listado con todos los productos, ordenados por categoría, en el que se muestre solo las tres primeras letras de la categoría.
+5.Obtener el cuadrado y el cubo de los precios de los productos.
+6.Devuelve la fecha del mes actual.
+7.Para los pedidos entregados el mismo mes que el actual, obtener cuantos días hace que se entregaron.
+8.En el nombre de los productos, sustituir “tarta” por “pastel”.
+9.Obtener la población del código postal (los primeros dos caracteres se refieren a la provincia y los tres últimos a la población).
+10.Obtén el número de productos de cada categoría, haz que el nombre de la categoría se muestre en mayúsculas.
+11.Obtén un listado de productos por categoría y dentro de cada categoría del nombre de producto más corto al más largo.
+12.Asegúrate de que los nombres de los productos no tengan espacios en blanco al principio o al final de dicho nombre.
+13.Lo mismo que en el ejercicio 2, pero ahora, además, sustituye el 4 y 5 número del resultado por las 2 últimas letras del nombre de la empresa.
+14.Obtén el 10 por ciento del precio de los productos redondeados a dos decimales.
+15.Muestra un listado de productos en que aparezca el nombre del producto y el código de producto y el de categoría repetidos (por ejemplo para la tarta de azucar se mostrará “623623”).
 */
+
+-- 1.Obtener todos los productos que comiencen por una letra determinada.
+delimiter $$
+drop procedure if exists ejercicio1 $$
+create procedure ejercicio1
+(
+	letra char
+)
+begin
+select  descripcion
+from productos
+where descripcion like concat(letra, '%');
+end $$
+delimiter ;
+
+
+call ejercicio1('C');
+
+/*
+2.Se ha diseñado un sistema para que los proveedores puedan acceder a ciertos datos, 
+la contraseña que se les da es el teléfono de la empresa al revés. 
+Se pide elaborar un procedimiento almacenado que dado un proveedor obtenga su contraseña y la muestre en los resultados.
+
+*/
+delimiter $$
+drop procedure if exists ejercicio2 $$
+create procedure ejercicio2
+(
+  codigoProveedor int
+	)
+    
+begin
+ select  reverse(cast(telefono as char)) 
+ from proveedores
+ where codigoProveedor = codproveedor;
+end $$
+delimiter ;
+
+
+call ejercicio2(1);
+
+-- 2.Obtener el mes previsto de entrega para los pedidos que no se han recibido aún y para una categoría determinada.
+
 
