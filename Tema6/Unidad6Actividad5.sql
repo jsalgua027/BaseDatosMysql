45bff1806aacf053a6d768701231f64c14b4f883
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-03-22 19:51:59 +0100

    terminado relacion 5 actividade tema 6

--- Tema6/Unidad6Actividad5.sql
+++ Tema6/Unidad6Actividad5.sql
@@ -112,23 +112,60 @@ call ejercicio3(1);
 
 
 -- 4.Obtener un listado con todos los productos, ordenados por categoría, en el que se muestre solo las tres primeras letras de la categoría.
+delimiter $$
+drop procedure if exists ejercicio4_5 $$
+create procedure ejercicio4_5()
+begin 
 select left(categorias.Nomcategoria,3) , productos.descripcion
 from productos
 join 
 categorias on productos.codcategoria = categorias.codcategoria
 order by categorias.codcategoria;
+end$$
+delimiter ;
+
+call ejercicio4_5();
 
 -- 5.Obtener el cuadrado y el cubo de los precios de los productos.
+delimiter $$
+drop procedure if exists ejercicio5_5 $$
+create procedure ejercicio5_5()
+begin 
 select pow(productos.precioUnidad,2) as precioAlCuadrado ,  pow(productos.precioUnidad,3) as precioAlCubo
 from productos;
+end$$
+delimiter ;
 
+call ejercicio5_5();
 -- 6.Devuelve la fecha del mes actual.
-select month(curdate()) as MesActual;
+delimiter $$
+drop function if exists ejercicio6_5$$
+create function ejercicio6_5()
+returns int
+deterministic
+begin
+declare mes int;
+ set mes =  month(curdate());
+ return mes;
+end$$
+delimiter ;
 
--- !!!!!! NO SALE  7.Para los pedidos entregados el mismo mes que el actual, obtener cuantos días hace que se entregaron.  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-select datediff(month(now()), fecentrega) as Dias
-from pedidos;
 
+
+
+select ejercicio6_5();
+--   7.Para los pedidos entregados el mismo mes que el actual, obtener cuantos días hace que se entregaron.  
+delimiter $$
+drop procedure if exists ejercicio7_5$$
+create procedure ejercicio7_5()
+begin
+select datediff(curdate(), fecentrega) as Dias
+from pedidos
+where month(pedidos.fecentrega)= 7; -- month(curdate())
+end$$
+delimiter ;
+
+call ejercicio7_5();
 
 -- !!!! 8.En el nombre de los productos, sustituir “tarta” por “pastel”. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -137,20 +174,124 @@ set descripcion = replace(descripcion , 'tarta', 'pastel');
 
 
 
--- 9.Obtener la población del código postal (los primeros dos caracteres se refieren a la provincia y los tres últimos a la población).
+-- 9.Obtener la población del código postal de los proveedores (los primeros dos caracteres se refieren a la provincia y los tres últimos a la población).
 
+delimiter $$
+drop procedure if exists ejercicio9_5 $$
+create procedure ejercicio9_5 ()
+begin
+select substring(proveedores.codpostal,3,5) as NumeroPoblacion
+from proveedores;
+end$$
+delimiter ;
+
+call ejercicio9_5();
 
 
 -- 10.Obtén el número de productos de cada categoría, haz que el nombre de la categoría se muestre en mayúsculas.!!!!!!!!!!
+delimiter $$
+drop procedure if exists ejercicio10_5$$
+create procedure ejercicio10_5()
+begin
 SELECT 
-    COUNT(productos.codcategoria) AS NumeroDeProductos,
-    LOWER(categorias.Nomcategoria) AS nombre
+    COUNT(productos.codproducto) AS NumeroDeProductos,
+    upper(categorias.Nomcategoria) AS nombre
 FROM
     productos
         JOIN
-    categorias ON categorias.codcategoria = productos.codcategoria;
+    categorias ON productos.codcategoria= categorias.codcategoria
+    group by categorias.Nomcategoria;
+end$$
+delimiter ;
 
+call ejercicio10_5();
+select count(productos.codproducto) from productos;
 
 -- 11.Obtén un listado de productos por categoría y dentro de cada categoría del nombre de producto más corto al más largo.
+delimiter $$
+drop procedure if exists ejercicio11_5$$
+create procedure ejercicio11_5()
+begin
+select productos.descripcion as nombre
+from productos
+	join 
+	categorias on productos.codcategoria= categorias.codcategoria
+    order by   length(productos.descripcion) ; -- order by   length(productos.descripcion) desc; tamaño del string en orden descendente 
+
+end$$
+delimiter ;
+
+call ejercicio11_5();
+
+
+-- 12.Asegúrate de que los nombres de los productos no tengan espacios en blanco al principio o al final de dicho nombre.
+delimiter $$
+drop procedure if exists ejercicio12_5$$
+create procedure ejercicio12_5()
+begin
+select trim(productos.descripcion) as nombre 
+from productos
+	join 
+	categorias on productos.codcategoria= categorias.codcategoria
+    order by   length(productos.descripcion) ; -- order by   length(productos.descripcion) desc; tamaño del string en orden descendente 
+
+end$$
+delimiter ;
+
+call ejercicio12_5();
+
+-- 13.Lo mismo que en el ejercicio 2, pero ahora, además, sustituye el 4 y 5 número del resultado por las 2 últimas letras del nombre de la empresa. ERROR
+delimiter $$
+drop procedure if exists ejercicio13_5 $$
+create procedure ejercicio13_5
+(
+  codigoProveedor int
+	)
+    
+begin
+declare letraEmpresa varchar(10);
+declare contrasenia varchar(10);
+set letraEmpresa = right(proveedores.nomempresa,2);
+set contrasenia = reverse(cast(telefono as char));
+ select reverse(cast(telefono as char)) as contraseña, concat(substring(contrasenia,1,3), letraEmpresa, concat(substring(contrasenia,6)))as sustitucion
+ from proveedores
+ where codigoProveedor = proveedores.codproveedor;
+end $$
+delimiter ;
+
+
+
+call ejercicio13_5(1);
+
+-- 14. Obtén el 10 por ciento del precio de los productos redondeados a dos decimales.
+
+delimiter $$
+drop procedure if exists ejercicio14_5$$
+create procedure ejercicio14_5()
+begin
+	select  truncate((productos.preciounidad*0.1),2) as productos10PorCiento -- truncate() dedondea a dos decimales
+    from productos;
+
+end$$
+delimiter ;
+
+call ejercicio14_5();
+
+
+/* Muestra un listado de productos en que aparezca el nombre del producto
+ y el código de producto y el de categoría repetidos (por ejemplo para la tarta de azucar se mostrará “623623”).
+*/
+
+delimiter $$
+drop procedure if exists ejercicio15_5$$
+create procedure ejercicio15_5()
+begin
+select productos.descripcion as nombreProducto , productos.codproducto 
+from productos
+where productos.codproducto= productos.codcategoria;
+end$$
+delimiter ;
+
+call ejercicio15_5();
 
 
