78d2c95c8c9172b541596adbc238a2a3d870e92d
jsalgua027 <jsalcedoguarde@gmail.com>
2023-04-24 12:02:15 +0200

    Añadido teoria Union del tema 7 y dos ejercicios propuestos

--- Tema7contTema6/teoriaTema7.sql
+++ Tema7contTema6/teoriaTema7.sql
@@ -160,6 +160,49 @@ create table centros_new
          select * 
          from  CATALOGOPRODUCTOS;
          
+         /*
+         -- versión eva
+				 create view catalogoprecios  
+
+		(referencia, descripcion, preciobase, precioHoy, categoria)
+
+				as
+
+
+					select refart, nomart, preciobase, precioventa, codcaat
+
+					from articulos
+
+		where refarticulo not in
+				
+			(select catalogospromos.refarticulo
+				 
+			from catalogospromos join promociones on catalogospromos.codpromo = promociones.codpromo	
+			where curdate() between promociones.fecinipromo
+			
+						and date_add (promociones.fecinipromo,
+		 interval promociones.duracion day)
+
+			)
+
+
+		union all /* se repiten */
+		-- union  /* no se repiten */
+
+
+select articulos.desarticulo, catalogospromos.precioventa
+
+from catalogospromos join promociones on catalogospromos.codpromo = promociones.codpromo	
+	where curdate() between promociones.fecinipromo
+	
+				and date_add (promociones.fecinipromo,
+ interval promociones.duracion day)
+
+         
+         */
+         
+         
+         
         /*
         para la bd empresaclase
         prepara una vista que se llamará LISTINTELEFONICO 	en la qye cada usuario podrá consultar
