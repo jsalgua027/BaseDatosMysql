6788933c17ff92608be1defbe2e0d4b1f6e5e23e
jsalgua027 <jsalcedoguarde@gmail.com>
2023-05-04 09:45:55 +0200

    parte de teoria expresiones regulares

--- Tema7contTema6/TeoriaCRUDyExpresionesRegu.sql
+++ Tema7contTema6/TeoriaCRUDyExpresionesRegu.sql
@@ -8,3 +8,18 @@
   delete --> delete
 
 */
+/*
+expresiones regulares
+
+operador LIKE
+
+selec *
+from centros
+where dirce like '%ATOCHA%'
+
+% --> cualquier cadena incluida la cadena vacia
+_ --> un caracter y solo uno en una posicion determinda (en la que posiciono el simbolo)
+
+^ --> que empiece por 
+$ --> que termine por
+*/
