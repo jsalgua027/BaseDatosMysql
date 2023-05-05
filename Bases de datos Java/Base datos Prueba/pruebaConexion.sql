9abed0370c8b6f922f255181433b8ee31a977506
jsalgua027 <jsalcedoguarde@gmail.com>
2023-05-05 10:42:42 +0200

    añadido la tabla persona a la base de datos pruebaConexion

--- Bases de datos Java/Base datos Prueba/pruebaConexion.sql
+++ Bases de datos Java/Base datos Prueba/pruebaConexion.sql
@@ -1,1 +1,12 @@
-create database pruebaConexion;
+-- create database pruebaConexion;
+DROP TABLE IF EXISTS `persona`;
+
+CREATE TABLE `persona` (
+  `pk` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `fecha_nac` datetime DEFAULT NULL,
+  PRIMARY KEY (`pk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+END 
+DELIMITER ;
