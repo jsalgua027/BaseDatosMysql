e9194fe33c6e015019c2e5e94eb7b9cec02fecef
jsalgua027 <jsalcedoguarde@gmail.com>
2023-05-16 11:11:39 +0200

    .......

---
+++ tema8/teoriaTema8.sql
@@ -0,0 +1,32 @@
+/*
+tema 8 teoria
+
+¿Tabla?
+¿Operacion?
+¿Antes/despues?alter
+before--> control  / after-->operacion adicional
+
+
+
+
+*/
+
+/*
+
+ventas
+-->comprobr si hay un stock suficient	
+-->quiero modificar el stock
+   
+-->cuando queden  menos de 5 unidadfes de stock quiero hacer pedido autoatico de cinco unidades
+
+before insert de aqui es donde hago la comprobacion detalleventas
+after insert detalleventa
+
+after update prodcutos
+
+
+    
+*/
+
+
+
